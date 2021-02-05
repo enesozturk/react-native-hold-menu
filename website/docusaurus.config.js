dc77bff63a9cb2ad9f14a7db59ae7ffb318cdb56
Enes Ozturk <enesozturk.d@gmail.com>
2021-02-06 01:59:41 +0300

    Update home page, add dynamic images

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,103 +1,55 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "My Site",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      title: "React Native Hold Menu",
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "examples", label: "Examples", position: "left" },
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/enesozturk/react-native-hold-menu",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
+      style: "dark",
+      links: [],
       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,129 +1,56 @@
 import React from "react";
-import clsx from "clsx";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import useThemeContext from "@theme/hooks/useThemeContext";
 
 import styles from "../css/header.module.css";
 import LightMenu from "@site/static/img/light-phone.png";
-import HoldItemAndMenu from "@site/static/img/hold-item-and-menu.png";
+import DarkImage from "@site/static/img/masked-dark-phone.png";
+import LightImage from "@site/static/img/masked-light-phone.png";
 
-const features = [
-  {
-    title: "Easy to Use",
-    imageUrl: "img/undraw_docusaurus_mountain.svg",
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: "Focus on What Matters",
-    imageUrl: "img/undraw_docusaurus_tree.svg",
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: "Powered by React",
-    imageUrl: "img/undraw_docusaurus_react.svg",
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
+function Content() {
+  const { isDarkTheme } = useThemeContext();
 
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx("col col--4", styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
-function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
   return (
     <header>
       <div className={styles.wrapper}>
-        <div className={styles.headerWrapper}>
-          <a className={styles.headerTitle}>React Native Hold Menu</a>
-          <nav className={styles.menu}>
-            <ul>
-              <li>
-                <a href="#">Documentation</a>
-              </li>
-              <li>
-                <a href="#">Examples</a>
-              </li>
-              <li>
-                <a href="#">Github</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
         <section className={styles.hero}>
           <div className={styles.heroWrapper}>
             <h1 className={styles.heading}>
               Make your components more interactable
             </h1>
             <h2 className={styles.subHeading}>
-              A performant, easy to use, accessible and extensible{" "}
+              A performant, easy to use and extensible{" "}
               <strong>hold to open context menu</strong> for React Native
               powered by Reanimated.
             </h2>
-            <div className={styles.flex}>
+            <div className={`${styles.flex} ${styles.buttonsWrapper}`}>
               <div className={styles.flexItem}>
                 <button className={styles.button}>Get Started</button>
               </div>
               <div className={styles.flexItem}>
                 <button className={styles.copyPaste}>
-                  npm install react-native-hold-menu
+                  yarn add react-native-hold-menu
                 </button>
               </div>
             </div>
           </div>
-          <div
-            style={{
-              flex: 2,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              position: "relative",
-            }}
-          >
-            <img src={LightMenu} style={{ maxHeight: 450 }} />
+          <div className={styles.phoneImageWrapper}>
             <img
-              src={HoldItemAndMenu}
-              style={{
-                position: "absolute",
-                top: 90,
-                right: 50,
-                width: 250,
-                height: 250,
-              }}
+              src={isDarkTheme ? DarkImage : LightImage}
+              style={{ maxHeight: 450 }}
             />
           </div>
         </section>
       </div>
     </header>
+  );
+}
+
+function Home() {
+  return (
+    <Layout>
+      <Content />
+    </Layout>
   );
 }
 
