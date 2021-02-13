29f5ce001a9516ad07399bfc6f3a54b5dbfa896b
Enes Ozturk <enesozturk.d@gmail.com>
2021-02-13 17:49:40 +0300

    Update prop types for hold item comps

--- src/components/holdItem/types.d.ts
+++ src/components/holdItem/types.d.ts
@@ -3,7 +3,7 @@ import Animated from 'react-native-reanimated';
 import { MenuItemProps } from '../../types';
 import { TransformOriginAnchorPosition } from '../../utils/calculations';
 
-export interface HoldItemProps {
+export interface HoldItemCommonProps {
   /**
    * Identifier for hold items. Must be unique.
    * @type string | number
@@ -38,20 +38,43 @@ export interface HoldItemProps {
   disableMove?: boolean;
 
   /**
-   *
-   * @type boolean
-   * @default false
+   * Hold item wrapper component styles.
+   * You may need for some examples like dynamic width or hight like message boxes.
+   * See Whatsapp example.
+   * @type ViewStyles
+   * @default {}
    * @examples
-   * disableMove={true}
+   * styles={{ maxWidth: '80%' }}
    */
   styles?: ViewStyle | ViewStyle[];
 
+  /**
+   * Callback function that need to be set and called when menu is toggled.
+   * @type () => void;
+   * @examples
+   * onActivate={()=>{ handleActivate(item.id) }}
+   */
   onActivate: () => void;
-  // isActive: boolean;
+
+  /**
+   * Theme for menu background and texts
+   * @type string
+   * @examples
+   * theme="light"
+   */
+  theme?: 'light' | 'dark';
+}
+
+export interface HoldItemProps extends HoldItemCommonProps {
+  /**
+   * Shared value with unique item ID that used to activate related item.
+   * @type Animated.SharedValue<number>
+   * @examples
+   * const active = useSharedValue<number>(0);
+   */
   active: Animated.SharedValue<number>;
 }
 
-export interface HoldItemChildProps extends HoldItemProps {
-  theme?: 'light' | 'dark';
+export interface HoldItemChildProps extends HoldItemCommonProps {
   isActive: Animated.SharedValue<boolean>;
 }
