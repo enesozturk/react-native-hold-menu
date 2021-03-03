c4c386b1d728916bca0798193b89f0871d1aa080
Enes Ozturk <enesozturk.d@gmail.com>
2021-03-03 21:23:11 +0300

    chore: enable icon rendering with seperator item

--- src/components/backdrop/constants.ts
+++ src/components/backdrop/constants.ts
@@ -1,2 +1,8 @@
-export const BACKDROP_LIGHT_BACKGROUND_COLOR = 'rgba(0,0,0,0.2)';
-export const BACKDROP_DARK_BACKGROUND_COLOR = 'rgba(0,0,0,0.75)';
+import { IS_IOS } from '../../constants';
+
+export const BACKDROP_LIGHT_BACKGROUND_COLOR = IS_IOS
+  ? 'rgba(0,0,0,0.2)'
+  : 'rgba(19, 19, 19, 0.95)';
+export const BACKDROP_DARK_BACKGROUND_COLOR = IS_IOS
+  ? 'rgba(0,0,0,0.75)'
+  : 'rgba(0,0,0,0.95)';
