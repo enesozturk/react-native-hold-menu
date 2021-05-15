aa87c700715290c7c657337263a69763a9b3f3d3
Enes <enesozturk.d@gmail.com>
2021-05-15 22:16:09 +0300

    fix: typo for separator word that missed on rebase (#26)
    * fix: typo for separator word that missed on rebase
    
    * chore: update wrong separator words

--- src/components/menu/Separator.tsx
+++ src/components/menu/Separator.tsx
@@ -5,23 +5,23 @@ import { useInternal } from '../../hooks';
 
 import { BORDER_LIGHT_COLOR, BORDER_DARK_COLOR } from './constants';
 
-const Seperator = () => {
+const Separator = () => {
   const { theme } = useInternal();
 
-  const seperatorStyles = useAnimatedStyle(() => {
+  const separatorStyles = useAnimatedStyle(() => {
     return {
       backgroundColor:
         theme.value === 'dark' ? BORDER_DARK_COLOR : BORDER_LIGHT_COLOR,
     };
   }, [theme]);
 
-  return <Animated.View style={[styles.seperator, { ...seperatorStyles }]} />;
+  return <Animated.View style={[styles.separator, { ...separatorStyles }]} />;
 };
 
-export default memo(Seperator);
+export default memo(Separator);
 
 const styles = StyleSheet.create({
-  seperator: {
+  separator: {
     width: '100%',
     height: 8,
   },
