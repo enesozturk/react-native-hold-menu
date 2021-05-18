12c669a9b51e12ad9ccc30e8fedd4ee1733f445a
刘力瑞 <admin@liulirui.com>
2021-05-18 16:53:17 +0800

    Set icon's color is same with text

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -65,10 +65,6 @@ const MenuItemComponent = ({ item, isLast }: MenuItemComponentProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state, item]);
 
-  const iconStyle = useAnimatedStyle(() => {
-    return { color: theme.value === 'light' ? 'black' : 'white' };
-  }, [theme]);
-
   return (
     <>
       <AnimatedTouchable
@@ -85,7 +81,7 @@ const MenuItemComponent = ({ item, isLast }: MenuItemComponentProps) => {
           {item.text}
         </Animated.Text>
         {!item.isTitle && item.icon && (
-          <AnimatedIcon name={item.icon} size={18} style={iconStyle} />
+          <AnimatedIcon name={item.icon} size={18} style={textColor} />
         )}
       </AnimatedTouchable>
       {item.withSeparator && <Separator />}
