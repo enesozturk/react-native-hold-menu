8bc071e2c5020361a9c7d9b402dd0cd6c4be9479
Enes Ozturk <enesozturk.d@gmail.com>
2022-07-19 16:23:02 +0300

    chore: remove bare workflow example

--- example/src/screens/Clubhouse/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './Clubhouse';
-export { default as ClubhouseNavButtonLeft } from './ClubhouseNavButtonLeft';
-export { default as ClubhouseNavButtonRight } from './ClubhouseNavButtonRight';
