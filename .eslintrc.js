586c049b14c445b028ac3900ef86a6a52a48e9f7
Sohel Islam Imran <76745339+SohelIslamImran@users.noreply.github.com>
2022-07-31 01:07:16 +0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     'no-console': ['error', { allow: ['warn', 'error'] }],
     'prettier/prettier': [
-      'warn',
+      'error',
       {
         printWidth: 80,
         arrowParens: 'avoid',
